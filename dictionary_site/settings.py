8af4fb9357a22984366d3470e1d475246706f2e5
Anton Dobrolyubov <antonpontiac909.com@gmail.com>
2022-11-07 17:44:36 +0300

    added blank for testing

--- dictionary_site/settings.py
+++ dictionary_site/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'words.apps.WordsConfig',
+    'tests.apps.TestsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
